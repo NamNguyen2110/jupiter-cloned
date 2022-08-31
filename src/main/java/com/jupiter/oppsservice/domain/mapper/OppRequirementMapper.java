d7f0a72f58688202e10be1c64310a65b076d3ca3
ManhTuan07 <109933336+ManhTuan07@users.noreply.github.com>
2022-08-31 14:27:34 +0700

    modify

--- src/main/java/com/jupiter/oppsservice/domain/mapper/OppRequirementMapper.java
+++ src/main/java/com/jupiter/oppsservice/domain/mapper/OppRequirementMapper.java
@@ -1,6 +1,6 @@
 package com.jupiter.oppsservice.domain.mapper;
 
-import com.jupiter.oppsservice.domain.dto.request.OppRequirementRequest;
+import com.jupiter.oppsservice.domain.dto.request.OppPositionRequest;
 import com.jupiter.oppsservice.domain.entity.OppPosition;
 import org.mapstruct.Mapper;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface OppRequirementMapper {
-    OppPosition toEntity(OppRequirementRequest request);
+    OppPosition toEntity(OppPositionRequest request);
 
-    List<OppPosition> toEntityList(List<OppRequirementRequest> request);
+    List<OppPosition> toEntityList(List<OppPositionRequest> request);
 }
